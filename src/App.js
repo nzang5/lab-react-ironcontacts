95af4c629f3ae77ca68d14097a18756bf28363c6
Nash Zangio <nash.c.zangio@gmail.com>
2022-08-30 18:28:46 +0200

    incomplete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import  allContacts from "./contacts.json";
+import React, { useState } from 'react';
+
+
+// Iteration 1
+const firstFive = allContacts.slice(0 , 5);
+console.log(firstFive);
+
+
+// iteration 3
+// const addRandom = () => {
+//   let randomCeleb = Math.floor(Math.random() * allContacts.length);
+//   if (!firstFive.includes(allContacts[i])){
+//     firstFive.push(allContacts);
+//   }
+//   setCelebs();
+// }
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [celebs, setCelebs] = useState(firstFive);
+
+  return <div className="App">
+
+<table>
+<h1>IronContacts</h1>
+<button> Add Random Contact</button>
+<br/>
+
+<tr>
+  <th>Picture</th>
+  <th>Name</th>
+  <th>Popularity</th>
+  <th>Won Oscar</th>
+  <th>Won Emmy</th>
+
+
+  </tr>
+
+{celebs.map(function(oneCeleb){
+
+  return(
+  <tr>
+  <td> <img src={oneCeleb.pictureUrl}/>   </td> 
+  <td>  {oneCeleb.name} </td>
+  <td>   {oneCeleb.popularity} </td>
+  <td>   {oneCeleb.wonOscar ? "🏆" : ""} </td>
+  <td>   {oneCeleb.wonEmmy ? "🏆" : ""} </td>
+</tr>
+)
+
+})
 }
+</table>
+
+
+  </div>;
+}
+
 
 export default App;
